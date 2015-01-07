6c3eea7abb336490091c2fa73eaedcf01b98cedc
lauraluca <laura.luca@clarisoft.com>
2015-01-07 23:16:45 +0200

    dashboard menu item available just for logged in

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -15,7 +15,9 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Actions<span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
-                        <li><a href="{{ URL::to('/dashboard') }}">Dashboard</a></li>
+                        @if (Auth::check())
+                            <li><a href="{{ URL::to('/dashboard') }}">Dashboard</a></li>
+                        @endif
                         <li><a href="#">View API reference</a></li>
                     </ul>
                 </li>
